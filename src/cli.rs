f3f847832baaa1afe0f90337034f0f72644c68c6
Spaghetoz <80428139+Spaghetoz@users.noreply.github.com>
2026-02-03 11:40:13 +0100

    [refactor] improve cli error propagation and readability

--- src/cli.rs
+++ src/cli.rs
@@ -1,19 +1,15 @@
+use std::error::Error;
 use std::io::{self, Write};
 
 mod interaction;
 
-use crate::cli::interaction::{Interaction, TerminalInteraction};
+use crate::cli::interaction::{Interaction, TerminalInteraction, UserInput};
 use crate::command::{IoFds};
 use crate::parsing::{convert_to_command};
 use crate::command::builtin::get_working_directory;
 
 pub fn run_cli() {
 
-    let terminal_fds = IoFds {
-        stdin: 0,
-        stdout: 1,
-        stderr: 2,
-    };
     let terminal = TerminalInteraction::new();
 
     println!(" ____            _     ____  _          _ _ ");
@@ -23,32 +19,46 @@ pub fn run_cli() {
     println!("|_| \\_\\\\__,_|___/\\__| |____/|_| |_|\\___|_|_|\n");
 
     loop {
-
-        let working_dir = get_working_directory().expect("get_working_directory failed"); // TODO handle error
-        // Prints a pretty colored shell prompt
-        print!("$ \x1b[1;34m{}\x1b[0m> ", working_dir);
-        // Flush stdout to directly print without using \n (since stdout is line-buffered)
-        io::stdout().flush().expect("stdout flush failed");  // TODO handle error
-
-        let user_input = match terminal.receive_input() {
-            Ok(input) => input,
-            Err(err) => {
-                println!("input error: {err}");
-                continue;
-            }
-        };
-
-        let input_command = match convert_to_command(&user_input) {
-            Ok(command) => command,
-            Err(err) => {
-                println!("Parsing error : {err}");
-                continue;
-            }
-        };
-        
-        if let Err(err) = input_command.execute(&terminal_fds) {
+        if let Err(err) = cli_loop_step(&terminal) {
             println!("{err}");
         }
-    
     }
+}
+
+/// Processes a single step on a loop
+pub fn cli_loop_step(terminal: &dyn Interaction) -> Result<(), Box<dyn Error>>{
+
+    print_prompt_string();
+
+    let user_input = terminal.receive_input()
+        // Propagate the error by specifying it is a user input error
+        .map_err(|e| Box::<dyn std::error::Error>::from(format!("Input error: {}", e)))?;
+
+    match user_input {
+        UserInput::String(input_string) => {
+            
+            let input_command = convert_to_command(&input_string)
+                .map_err(|e| Box::<dyn std::error::Error>::from(format!("Parsing error: {}", e)))?;
+            
+            input_command.execute(&IoFds {stdin: 0, stdout: 1, stderr: 2})
+                .map_err(|e| Box::<dyn std::error::Error>::from(format!("Execution error: {}", e)))?; 
+
+        },
+        UserInput::Interruption => todo!(), 
+    }
+
+    Ok(())
+}
+
+pub fn print_prompt_string() {
+    
+    let working_dir = get_working_directory()
+        .unwrap_or_else(|_| String::from("unknown"));
+
+    // Prints a pretty colored shell prompt
+    print!("$ \x1b[1;34m{}\x1b[0m> ", working_dir);
+    
+    // Flush stdout to directly print without using \n (since stdout is line-buffered)
+    io::stdout().flush().expect("stdout flush failed");  // TODO handle error
+
 }
--- src/cli/interaction.rs
+++ src/cli/interaction.rs
@@ -7,8 +7,13 @@ use std::{error::Error, io};
 
 /// Represents the contract that an interaction with the user should respect
 pub trait Interaction {
-    fn receive_input(&self) -> Result<String, Box<dyn Error>>;
+    fn receive_input(&self) -> Result<UserInput, Box<dyn Error>>;
     fn save_history(&self) -> Result<(), Box<dyn Error>>;
+}
+/// Represents what a user input could be, it could be just a string, or an action 
+pub enum UserInput {
+    String(String),
+    Interruption, // ctrl c
 }
 
 
@@ -26,17 +31,13 @@ impl TerminalInteraction {
 impl Interaction for TerminalInteraction {
 
     /// Returns the String entered by the user on the stdin
-    fn receive_input(&self) -> Result<String, Box<dyn Error>> {
+    fn receive_input(&self) -> Result<UserInput, Box<dyn Error>> {
 
         let mut input = String::new();
-        
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line");
-
+        io::stdin().read_line(&mut input)?;
         input = input.trim().to_string();
 
-        Ok(input)
+        Ok(UserInput::String(input))
         
     }
 
